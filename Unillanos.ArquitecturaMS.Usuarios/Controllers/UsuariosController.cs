ab81e10dcbea08eb64aa6fad8b6f619d3f02841f
ItsMario25 <mmariohernandez58@gmail.com>
2022-02-19 14:51:12 -0500

    ss

--- Unillanos.ArquitecturaMS.Usuarios/Controllers/UsuariosController.cs
+++ Unillanos.ArquitecturaMS.Usuarios/Controllers/UsuariosController.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using System;
 using System.IO;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Unillanos.ArquitecturaMS.Usuarios.Controllers
 {
@@ -13,50 +14,111 @@ namespace Unillanos.ArquitecturaMS.Usuarios.Controllers
     {
         [HttpGet]
         [Route("LeerPerfil/{id}")]
-
         public string Get(int id)
         {
-            return id switch
+            string path = @"C:\Windows\Temp\user" + id + ".json";
+
+            if (System.IO.File.Exists(path))
             {
-                1 => "Mario",
-                2 => "Curso",
-                _ => throw new System.NotSupportedException("El id no es valido")
-            };
+                StreamReader r = new StreamReader(path);
+                string jsonString = r.ReadToEnd();
+                r.Close();
+                return jsonString;
+            } else {
+                return "No existe informacion del usuario";
+            }
+
         }
+
 
         [HttpPost]
         [Route("InsertUsuario")]
         public string Post(UsuariosDto usuario)
         {
-            string path = @"C:\Windows\Temp\user1.json";
+            int id = 1;
+            string path = @"C:\Windows\Temp\user" + id + ".json";
 
-            // Delete the file if it exists.
-            if (System.IO.File.Exists(path))
+            while (System.IO.File.Exists(path))
             {
-                System.IO.File.Delete(path);
+                id = id + 1;
+                path = @"C:\Windows\Temp\user" + id + ".json";
             }
-            string jsonString = JsonSerializer.Serialize(usuario);
-            System.IO.File.WriteAllText(path, jsonString);
-
-
-            return usuario.Nombre;
+            string jsonS = System.Text.Json.JsonSerializer.Serialize(usuario);
+            System.IO.File.WriteAllText(path, jsonS);
+            return usuario.Nombre+" Ingresado exitosamente";
         }
 
+
         [HttpPut]
-        [Route("AddUsuario")]
-        public string Put(UsuariosDto user)
+        [Route("AddDate/{id}/{tipe}/{info}")]
+        public string Put(int id, string tipe, string info)
         {
-            string path = @"C:\Windows\Temp\user1.json";
-            return user.Nombre;
+            string path = @"C:\Windows\Temp\user"+id+".json";
+            if (System.IO.File.Exists(path))
+            {
+                StreamReader r = new StreamReader(path);
+                string jsonString = r.ReadToEnd();
+                r.Close();
+                UsuariosDto m = JsonConvert.DeserializeObject<UsuariosDto>(jsonString);
+                switch (tipe)
+                {
+                    case "Nombre" :
+                        m.Nombre = info;
+                        string jsonS = System.Text.Json.JsonSerializer.Serialize(m);
+                        System.IO.File.WriteAllText(path, jsonS);
+                        return "Nombre cambiado";
+                    case "Apellido":
+                        m.Apellido = info;
+                        string jsonS1 = System.Text.Json.JsonSerializer.Serialize(m);
+                        System.IO.File.WriteAllText(path, jsonS1);
+                        return "Apellido cambiado";
+                    case "Sexo":
+                        m.Sexo = info;
+                        string jsonS2 = System.Text.Json.JsonSerializer.Serialize(m);
+                        System.IO.File.WriteAllText(path, jsonS2);
+                        return "Sexo cambiado";
+                    case "Correo":
+                        m.Correo = info;
+                        string jsonS3 = System.Text.Json.JsonSerializer.Serialize(m);
+                        System.IO.File.WriteAllText(path, jsonS3);
+                        return "Correo cambiado";
+                    case "Telefono":
+                        m.Telefono = info;
+                        string jsonS4 = System.Text.Json.JsonSerializer.Serialize(m);
+                        System.IO.File.WriteAllText(path, jsonS4);
+                        return "Telefono cambiado";
+                    case "Edad":
+                        m.Edad = info;
+                        string jsonS5 = System.Text.Json.JsonSerializer.Serialize(m);
+                        System.IO.File.WriteAllText(path, jsonS5);
+                        return "Edad cambiado";
+                    default:
+                        return "Informacion de entrada incorrecta";
+                }
+            }else
+            {
+                return "Informacion de usuario no existente";
+            }
         }
 
 
         [HttpDelete]
-        [Route("AddUsuario")]
-        public string Delete(UsuariosDto user)
+        [Route("EliminarPerfil/{id}")]
+        public string Delete(int id)
         {
-            return "gelo";
+            string usser = "user" + id + ".json";
+            string path = @"C:\Windows\Temp\"+usser;
+
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+                return "Usuario eliminado";
+            } else
+            {
+                return "No existe";
+            }
         }
+
 
         public class UsuariosDto
         {
